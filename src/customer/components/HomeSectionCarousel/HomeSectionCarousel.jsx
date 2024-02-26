6e6cc8f4d00f4f7bedfd927b71defb5709b9c07c
Musaddik <musaddikkhan@gmail.com>
2024-02-26 22:22:44 +0530

    Product Card Section Changed

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,99 +1,77 @@
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
-import { Button } from "@mui/material";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { useState } from "react";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { Button } from "@mui/material";
 
-const HomeSectionCarousel = ({data }) => {
+const HomeSectionCarousel = ({data}) => {
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 4.5 },
+  };
 
   const slidePrev = () => setActiveIndex(activeIndex - 1);
   const slideNext = () => setActiveIndex(activeIndex + 1);
-  const syncActiveIndex = ({ item }) => setActiveIndex(item);
 
-  const responsive = {
-    0: {
-      items: 2,
-      itemsFit: "contain",
-    },
-    568: {
-      items: 3,
-      itemsFit: "contain",
-    },
-    1024: {
-      items: 5.5,
-      itemsFit: "contain",
-    },
-  };
-  const items = data?.slice(0, 10).map((item) => (
-    <div className="">
-      {" "}
-      <HomeSectionCard product={item} />
-    </div>
-  ));
+  const syncAcitveIndex = ({ item }) => setActiveIndex(item);
 
-  // const slideInFromRight = (t) => {
-  //   return `translateX(${100 - t * 100}%)`;
-  // };
-
+  const items = data.slice(0,10).map((item) => <HomeSectionCard product={item}/>);
   return (
-    <div className="relative px-4 sm:px-6 lg:px-8 ">
-      <div className="relative border p-5">
+    <div className="border">
+      <div className="relative p-5 border">
         <AliceCarousel
-          disableButtonsControls
-          disableDotsControls
-          mouseTracking
           items={items}
-          activeIndex={activeIndex}
+          disableButtonsControls
           responsive={responsive}
-          onSlideChanged={syncActiveIndex}
-          animationType="fadeout"
-          animationDuration={2000}
+          disableDotsControls
+          onSlideChange={syncAcitveIndex}
+          activeIndex={activeIndex}
         />
-        {activeIndex !== items.length - 5 && (
+        {(activeIndex !== items.length - 4)&& 
           <Button
-            onClick={slideNext}
             variant="contained"
-            className="z-50 bg-[]"
+            className="z-50 bg-white"
+            onClick={slideNext}
             sx={{
               position: "absolute",
               top: "8rem",
               right: "0rem",
               transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
             }}
-
             aria-label="next"
           >
-            <ArrowForwardIosIcon
-              className=""
-              sx={{ transform: "rotate(-90deg)" }}
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
             />
           </Button>
-        )}
+        }
 
-        {activeIndex !== 0 && (
+        {activeIndex !== 0 && 
           <Button
-            onClick={slidePrev}
             variant="contained"
-            className="z-50 bg-[]"
-    
+            className="z-50 bg-white"
+            onClick={slidePrev}
             sx={{
               position: "absolute",
               top: "8rem",
               left: "0rem",
-              transform: "translateX(-50%)  rotate(90deg)",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
             }}
             aria-label="next"
           >
-            <ArrowForwardIosIcon
-              className=""
-              sx={{ transform: " rotate(90deg)" }}
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
             />
           </Button>
-        )}
+        }
       </div>
     </div>
   );
 };
 
-export default  HomeSectionCarousel;
+export default HomeSectionCarousel;
