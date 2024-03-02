3f5d7d0e5129711031f0dc1cf45b9847a9de7478
Musaddik <musaddikkhan@gmail.com>
2024-03-02 22:09:57 +0530

    ProductDetail and Review Page Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import Navigation from './customer/components/Navigation/Navigation'
 // import Homepage from "./customer/Pages/Homepage/Homepage";
 import Footer from "./customer/components/Footer/footer";
-import Product from "./customer/components/Product/product";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
 
 
 
@@ -16,7 +16,8 @@ function App() {
          <Navigation/>
        <div>
           {/* <Homepage/> */} 
-          <Product/>
+          {/* <Product/> */}
+          <ProductDetails/>
        </div>
        <Footer/>
     </div>
