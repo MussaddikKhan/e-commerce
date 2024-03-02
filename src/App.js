f74fb8015174fec3220588e1d4a4cccbb52964f8
Musaddik <musaddikkhan@gmail.com>
2024-03-02 10:50:47 +0530

    Product Card Created With Filter Functinality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import React from "react";
 import "./App.css";
 
 import Navigation from './customer/components/Navigation/Navigation'
-import Homepage from "./customer/Pages/Homepage/Homepage";
+// import Homepage from "./customer/Pages/Homepage/Homepage";
 import Footer from "./customer/components/Footer/footer";
+import Product from "./customer/components/Product/product";
+
+
+
 
 function App() {
   return (
     <div className="">
          <Navigation/>
        <div>
-          <Homepage/>
+          {/* <Homepage/> */} 
+          <Product/>
        </div>
        <Footer/>
     </div>
--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -4,7 +4,7 @@ import { mainCarouselData } from './mainCarouselData'
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 const MainCarousel = () => {
-    const items  = mainCarouselData.map((item) => <img className='cursor-pointer' role='presentation' src={item.imageUrl} alt="Image Not Working"/>)
+    const items  = mainCarouselData.map((item) => <img className='cursor-pointer' role='presentation' src={item.imageUrl} alt=""/>)
   return (
     <AliceCarousel
         items={items}
